54afed1db26af8da63f914ae5f18df3554c47081
Aaron Dunn <aaron.dunn@accenture.com>
2019-04-23 14:44:00 -0400

    characters can now attack, damage, and die

--- Python/EverCraft/Character.py
+++ Python/EverCraft/Character.py
@@ -2,4 +2,24 @@
 class Character:
 
     def __init__(self, name=None):
+        self.is_alive = True
         self.name = name if name is not None else "Stranger"
+        self.health = 5
+        self.armor = 10
+
+    def attack(self, target_character, roll):
+        self._apply_standard_hit_damage(roll, target_character)
+        self._apply_critical_hit_damage(roll, target_character)
+        target_character._update_is_alive()
+
+    def _apply_standard_hit_damage(self, roll, target_character):
+        if roll >= target_character.armor:
+            target_character.health -= 1
+
+    def _apply_critical_hit_damage(self, roll, target_character):
+        if roll == 20:
+            target_character.health -= 1
+
+    def _update_is_alive(self):
+        if self.health <= 0:
+            self.is_alive = False
--- Python/EverCraftTests/CharacterTests.py
+++ Python/EverCraftTests/CharacterTests.py
@@ -12,3 +12,41 @@ class CharacterTest(unittest.TestCase):
         character = Character()
 
         self.assertEqual("Stranger", character.name)
+
+    def test_attack_roll_must_be_greater_than_or_equal_armor_to_hit(self):
+        character1 = Character()
+        character2 = Character()
+
+        roll = 10
+
+        character1.attack(character2, roll)
+
+        self.assertEqual(4, character2.health)
+
+    def test_attack_deals_double_damage_when_twenty_is_rolled(self):
+        character1 = Character()
+        character2 = Character()
+
+        roll = 20
+
+        character1.attack(character2, roll)
+
+        self.assertEqual(3, character2.health)
+
+    def test_character_is_dead_when_health_at_or_below_zero(self):
+        character1 = Character()
+        character2 = Character()
+
+        self.assertTrue(character2.is_alive)
+
+        self.kill_character(character1, character2)
+
+        self.assertFalse(character2.is_alive)
+
+
+    def kill_character(self, character1, character2):
+        roll = character2.armor + 1
+
+        for i in range(character2.health):
+            character1.attack(character2, roll)
+
